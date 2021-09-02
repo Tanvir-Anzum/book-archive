4c08fe8d3c02282f703b9e283747e92635ceded1
Tanvir Anzum <sshihab133075@gmail.com>
2021-09-02 22:35:20 +0600

    error handinlind solved using try and catch

--- bookLibrary.js
+++ bookLibrary.js
@@ -3,6 +3,8 @@
 const show = document.getElementById('show')
 const displaySpinner = document.getElementById('display-spinner')
 const showTextOutput = document.getElementById('show-text-output')
+let book = document.getElementById('book')
+let author = document.getElementById('author')
 
 // function for loading data
 const loadData = () => {
@@ -21,6 +23,7 @@ const loadData = () => {
   fetch(`https://openlibrary.org/search.json?q=${result}`)
     .then((res) => res.json())
     .then((data) => display(data.docs, result))
+    // .then((data) => console.log(data.docs))
 
   searchResult.value = ''
 }
@@ -49,20 +52,27 @@ const display = (inputArray, inputValue) => {
 
     // showing seven(7) results
     if(i<8){
-    const div = document.createElement('div')
-    div.innerHTML = `<div class="col mt-3">
-    <div class='card' style='width: 21rem;height: 520px; border: 2px solid black'>
-    <img  height="410px" src='https://covers.openlibrary.org/b/id/${input.cover_i}-M.jpg' class='card-img-top' alt='...' />
-      <div class='card-body'>
-      <div class="d-flex">
-        <h6 id="firstOutput" class='card-title me-4'>book name: ${input.title}</h6>
-        <h6 id="secondOutput" class='card-title'>author: ${input.author_name[0]}</h6>
-      </div>  
-      </div>
-    </div>
-    </div>
+    
+     const div = document.createElement('div')
+     try{
+     div.innerHTML = `<div class="col mt-3">
+     <div class='card' style='width: 21rem;height: 520px; border: 2px solid black'>
+     <img  height="410px" src='https://covers.openlibrary.org/b/id/${input.cover_i}-M.jpg' class='card-img-top' alt='...' />
+       <div class='card-body'>
+     <div class="d-flex">
+         <h6 id="firstOutput" class='card-title me-4'>book name: ${input.title}</h6>
+         <h6 id="secondOutput" class='card-title me-4'>book name: ${input.author_name[0]}</h6>
+       </div>
+        <h6 class='card-title me-4'>book name: ${input.publish_year[0]}
+       </div>
+     </div>
+     </div>
     `
-     show.appendChild(div)
+     }
+     catch{
+        console.log('successful use of try and catch')
+     }
+    show.appendChild(div)
   }   
   })
 }
